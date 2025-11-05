41483773dbe1c1e4b72fe345b61489ba8c37b713
amiefeng <147661385+amiefeng@users.noreply.github.com>
2025-11-05 12:33:29 -0500

    Updating UI for Expiration page
    Card items & colors are updated with templateColors.ts. Additionally, a dark mode version has been added to the general page.

---
+++ app/auth/login 2.tsx
@@ -0,0 +1,211 @@
+import React, { useEffect, useState } from 'react';
+import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+import { supabase } from '@/lib/supabase';
+import { Session } from '@supabase/supabase-js';
+import { router } from 'expo-router';
+import 'react-native-url-polyfill/auto';
+import validatePassword from './passwordValidator';
+
+
+export default function LoginScreen() {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  async function signInWithEmail() {
+    setLoading(true)
+    const { error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    })
+
+    if (error) Alert.alert(error.message)
+    setLoading(false)
+  }
+  
+  const [passwordError, setPasswordError] = useState('');
+
+  const handlePasswordChange = (text: string) => {
+    setPassword(text);
+    setPasswordError(''); //clear previous error message
+    
+    // authentilize current input password
+    if (text.length > 0) {
+      const { isValid, errorMessage } = validatePassword(text);
+      if (!isValid) {
+        setPasswordError(errorMessage);
+      }
+    }
+  };
+
+
+  const handleLogout = async () => {
+    const { error } = await supabase.auth.signOut()
+
+    if (error) {
+      console.error('Error logging out:', error.message)
+    } else {
+      console.log('Successfully logged out')
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>👤 Login</Text>
+      
+      {/* The logout button is only displayed when the user is logged in. */}
+      {session && session.user ? (
+        <View style={styles.loggedInContainer}>
+          <Text style={styles.loggedInText}>User Name: {session.user.user_metadata.display_name}</Text>
+          <Text style={styles.loggedInText}>You are already logged in</Text>
+          <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+            <Text style={styles.logoutButtonText}>Logout</Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <>
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            value={email}
+            onChangeText={setEmail}
+            autoCapitalize="none"
+          />
+
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            value={password}
+            secureTextEntry
+            onChangeText={handlePasswordChange}//judge password
+          />
+          
+          {passwordError ? (// display error message
+            <View style={styles.errorContainer}>
+              <Text style={styles.errorText}>{passwordError}</Text>
+            </View>
+          ) : null}
+
+          <TouchableOpacity style={styles.button} onPress={signInWithEmail}>
+            <Text style={styles.buttonText}>Sign In</Text>
+          </TouchableOpacity>
+        </>
+      )}
+
+      <View style={styles.flexSpacer} />
+
+      {/*jump to sign up*/}
+      <TouchableOpacity onPress={() => router.push('/auth/signup')}>
+        <Text style={styles.linkText}>
+          Don't have an account? <Text style={styles.linkHighlight}>Sign up</Text>
+        </Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+  flex: 1,
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  backgroundColor: '#fff',
+  paddingTop: 120, 
+  paddingBottom: 50,
+},
+  title: { 
+    fontSize: 28, 
+    fontFamily:'Avenir',
+    fontWeight: 'bold', 
+    marginBottom: 24, 
+    color: '#371B34',
+    textAlign: 'center' 
+},
+  loggedInContainer: {
+    width: '80%',
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+  loggedInText: {
+    fontSize: 18,
+    color: '#371B34',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  input: {
+    width: '80%',
+    borderWidth: 2,
+    borderColor: '#CDD0E3',
+    borderRadius: 8,
+    padding: 15, 
+    marginVertical: 12,
+    color: '#371B34',
+    backgroundColor: '#EBF6F0',
+    fontSize: 18,
+},
+  errorContainer: {
+    width: '80%',
+    backgroundColor: '#f8e3e3ff',
+    borderColor: '#FFBFBF',
+    borderRadius: 8,
+    borderWidth: 2,
+    padding: 12,
+    marginVertical: 12,
+  },
+  errorText: {
+    color: '#f54a4aff',
+    fontSize: 14,
+    fontWeight: '500',
+  },
+  button: { 
+    backgroundColor: '#371B34',
+    padding: 15, 
+    borderRadius: 8, 
+    alignItems: 'center',
+    width: '80%',
+    marginTop: 12,
+},
+  buttonText: { 
+    color: '#fff', 
+    fontSize: 18, 
+    fontWeight: '600' 
+},
+  logoutButton: {
+    backgroundColor: '#f54a4aff',
+    padding: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+    width: '100%',
+  },
+  logoutButtonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: '600'
+  },
+  linkText: { 
+    fontSize:16,
+    marginTop: 20, 
+    color: '#A598A4' 
+},
+  linkHighlight: { 
+    color: '#371B34',
+    fontWeight: 'bold' 
+},
+  flexSpacer: {
+    flex: 1,
+},
+});
---
+++ app/auth/signup 2.tsx
@@ -0,0 +1,330 @@
+import { supabase } from '@/lib/supabase';
+import { Ionicons } from '@expo/vector-icons';
+import { Session } from '@supabase/supabase-js';
+import { useRouter } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import validatePassword from '../auth/passwordValidator';
+
+
+export default function SignUpScreen() {
+  const router = useRouter();
+
+  const [username, setUsername] = useState('');
+  const [usernameError, setUsernameError] = useState('');
+
+  const [email, setEmail] = useState('');
+
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [confirmPasswordError, setConfirmPasswordError] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const validateUsername = (text: string) => {
+  //check if username is at most 20 characters long
+  if (text.length > 20) {
+    return false;
+  }
+  //check if username contains only alphanumeric characters
+  const regex = /^[a-zA-Z0-9]+$/;
+  return regex.test(text);
+};
+
+  const handleUsernameChange = (text: string) => {
+    //limit username to 20 characters
+    if (text.length <= 20) {
+      setUsername(text);
+    }
+  
+  setUsernameError('');
+  if (text.length > 0) {
+    if (!validateUsername(text)) {
+      setUsernameError('Username can only contain letters and numbers and must be no longer than 20 characters');
+    }
+  }
+};
+  const handlePasswordChange = (text: string) => {
+    setPassword(text);
+    setPasswordError(''); //clear previous error message
+    
+    // authenticate current input password
+    if (text.length > 0) {
+      const { isValid, errorMessage } = validatePassword(text);
+      if (!isValid) {
+        setPasswordError(errorMessage);
+      }
+    }
+  };
+
+  const handleConfirmPasswordChange = (text: string) => {
+    setConfirmPassword(text);
+    setConfirmPasswordError('');
+    
+    if (text !== password) {
+      setConfirmPasswordError('Passwords do not match');
+    }
+  };
+
+
+  const [session, setSession] = useState<Session | null>(null)
+  
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  const [loading, setLoading] = useState(false)
+
+  const handleSignup = async () => {
+    setLoading(true)
+    
+    if (!validateUsername(username)) {
+      Alert.alert(
+        'Invalid Username',
+        'Username must be less than 20 characters and contain only letters and numbers.',
+        [{ text: 'OK' }]
+      );
+      return;
+    }
+
+    // Validate password
+    const { isValid, errorMessage } = validatePassword(password);
+    if (!isValid) {
+      Alert.alert(
+        'Invalid Password',
+        errorMessage,
+        [{ text: 'OK' }]
+      );
+      return;
+    }
+    
+    // Check if passwords match
+    if (password !== confirmPassword) {
+      Alert.alert(
+        'Password Mismatch',
+        'Passwords do not match',
+        [{ text: 'OK' }]
+      );
+      return;
+    }
+    
+    const {
+      data: { session },
+      error,
+    } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+      options: {
+      data: {
+          display_name: username,
+        }
+      }
+    })
+
+    if (error) Alert.alert(error.message)
+    if (!error) {
+      const { error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password,
+      })
+
+      if (error) Alert.alert(error.message)
+
+      router.push('/(tabs)');
+    } 
+    setLoading(false)
+
+    // TODO: wait to replace with navigation back or success message
+  };
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Create Account</Text>
+      
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        autoCapitalize="none"
+        value={username}
+        onChangeText={handleUsernameChange}
+        maxLength={20}
+      />
+      {usernameError ? (
+        <View style={styles.errorContainer}>
+          <Text style={styles.errorText}>{usernameError}</Text>
+        </View>
+      ) : null}
+
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        value={email}
+        onChangeText={setEmail}
+      />
+
+      {/* Password Input with Eye Icon */}
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.inputWithIcon}
+          placeholder="Password"
+          value={password}
+          secureTextEntry={!showPassword}
+          onChangeText={handlePasswordChange}
+        />
+        <TouchableOpacity 
+          style={styles.eyeIcon} 
+          onPress={() => setShowPassword(!showPassword)}
+        >
+          <Ionicons 
+            name={showPassword ? "eye":"eye-off" } 
+            size={24} 
+            color="#371B34" 
+          />
+        </TouchableOpacity>
+      </View>
+      
+      {passwordError ? (
+        <View style={styles.errorContainer}>
+          <Text style={styles.errorText}>{passwordError}</Text>
+        </View>
+      ) : null}
+      
+      {/* Confirm Password Input with Eye Icon */}
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.inputWithIcon}
+          placeholder="Confirm Password"
+          secureTextEntry={!showConfirmPassword}
+          value={confirmPassword}
+          onChangeText={handleConfirmPasswordChange}
+        />
+        <TouchableOpacity 
+          style={styles.eyeIcon} 
+          onPress={() => setShowConfirmPassword(!showConfirmPassword)}
+        >
+          <Ionicons 
+            name={showConfirmPassword ? "eye":"eye-off" } 
+            size={24} 
+            color="#371B34" 
+          />
+        </TouchableOpacity>
+      </View>
+      
+      {confirmPasswordError ? (
+        <View style={styles.errorContainer}>
+          <Text style={styles.errorText}>{confirmPasswordError}</Text>
+        </View>
+      ) : null}
+
+      <TouchableOpacity style={styles.signupButton} onPress={handleSignup}>
+        <Text style={styles.signupText}>Sign Up</Text>
+      </TouchableOpacity>
+
+      <View style={styles.flexSpacer} />
+
+      {/* jump back to login */}
+      <TouchableOpacity onPress={() => router.back()}>
+        <Text style={styles.linkText}>
+          Already have an account? <Text style={styles.linkHighlight}>Log in</Text>
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+  flex: 1,
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  backgroundColor: '#fff',
+  paddingTop: 120,
+  paddingBottom: 50,
+},
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    fontFamily:'Avenir',
+    marginBottom: 30,
+    color: '#371B34',
+  },
+  input: {
+    width: '80%',
+    borderWidth: 2,
+    borderColor: '#CDD0E3',
+    borderRadius: 8,
+    padding: 15, 
+    marginVertical: 12,
+    color: '#371B34',
+    backgroundColor: '#EBF6F0',
+    fontSize: 16,
+},
+  inputContainer: {
+    width: '80%',
+    borderWidth: 2,
+    borderColor: '#CDD0E3',
+    borderRadius: 8,
+    marginVertical: 12,
+    backgroundColor: '#EBF6F0',
+    flexDirection: 'row',
+    alignItems: 'center',
+},
+  inputWithIcon: {
+    flex: 1,
+    padding: 15,
+    color: '#371B34',
+    fontSize: 16,
+},
+  eyeIcon: {
+    padding: 15,
+},
+  signupButton: {
+    backgroundColor: '#371B34',
+    padding: 15,
+    borderRadius: 8,
+    width: '80%',
+    marginTop: 20,
+  },
+  signupText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  linkText: {
+    fontSize:16,
+    marginTop: 20,
+    color: '#A598A4',
+    //marginBottom: 30, 
+  },
+  errorContainer: {
+    width: '80%',
+    backgroundColor: '#f8e3e3ff',
+    borderColor: '#FFBFBF',
+    borderRadius: 8,
+    borderWidth: 2,
+    padding: 12,
+    marginVertical: 12,
+  },
+  errorText: {
+    color: '#f54a4aff',
+    fontSize: 14,
+    fontWeight: '500',
+  },
+  linkHighlight: {
+    color: '#371B34',
+    fontWeight: 'bold',
+  },
+  flexSpacer: {
+    flex: 1,
+},
+});
